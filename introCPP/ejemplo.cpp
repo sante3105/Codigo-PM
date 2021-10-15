44fd64aa922ebbc80d2c770e30501000c38beede
Santiago Escobar <saescobarc@unal.edu.com>
2021-10-15 09:02:05 -0500

    Guardando carpeta de codigos c++ y primer codigo creado

---
+++ introCPP/ejemplo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{int a{3};
+  float x(2.2);
+  double y=1.2;
+  std::cout<< "hola mundo ";
+  std::cout<<"a="<<a<<"\n";
+  std::cout<<"x="<<x<<"\n";
+  std::cout<<"y="<<y<<"\n";
+  return 0;
+}
